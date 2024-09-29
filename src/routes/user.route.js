821f67027e6a68d190eb1639250b2ead72a95661
MegaArasan <megaarasan2000@gmail.com>
2024-09-30 01:07:21 +0530

    fix: lint issue

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { login } from '../controllers/user.controller.js';
 
+// eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.post('/login', login);
